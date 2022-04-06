307f3d06d2f0c406ac641fc60fe6b86331b2eb7b
Enrico <73340659+Enrypase@users.noreply.github.com>
2022-04-06 13:11:08 +0200

    Update 1.js

--- JavaScript/1/1.js
+++ JavaScript/1/1.js
@@ -1,6 +1,17 @@
-// Variable
-let count = 0
-// Print the variable in the browser's console
-console.log("Count: " + count)
-// Change the value of an elementù
-document.getElementById("counter").innerText = 5
+// Tutorial - Lession 01
+count = 0
+let counterElement = document.getElementById("counter")
+let savedElement = document.getElementById("savedItems")
+function increment() {
+    count++;
+    console.log("New count: " + count)
+    counterElement.textContent = count
+}
+function save() {
+    console.log("Count Saved: " + count)
+    let separator = " - "
+    if(savedElement.innerText == null || savedElement.innerText.length == 0){
+        separator = ""
+    }
+    savedElement.textContent =  savedElement.innerHTML + separator + count 
+}
