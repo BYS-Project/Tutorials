074404afe6799d25578f262b05dbcdd5b7e1def6
Enrico <73340659+Enrypase@users.noreply.github.com>
2022-04-06 11:50:05 +0200

    Create 1.js

---
+++ JavaScript/1/1.js
@@ -0,0 +1,6 @@
+// Variable
+let count = 0
+// Print the variable in the browser's console
+console.log("Count: " + count)
+// Change the value of an elementù
+document.getElementById("counter").innerText = 5
