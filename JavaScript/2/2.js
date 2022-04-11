e334de09e304015fc77c706b1d7d3c6150326f47
Enrico <73340659+Enrypase@users.noreply.github.com>
2022-04-11 12:35:27 +0200

    Update 2.js

--- JavaScript/2/2.js
+++ JavaScript/2/2.js
@@ -7,21 +7,38 @@
 */
 
 let cardsElement = document.getElementById("cards")
-let sumElement = document.getElementById("sum")
+// If it is an id "#" otherwise if it is a class ".". Linke in CSS
+let sumElement = document.querySelector("#sum")
 
-let card1 = 4
-let card2 = 5
-let sum = card1 + card2
+// array.push -> insert an element into the array in the last index of the array
+// array.pop -> deletes the last element of an array
+let cards = []
 
 let won = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
-function startGame() {
-    if(sum < 21){
+let player = {
+    name: "Player_Name",
+    chips: 145
+}
+
+function startGame(){
+    isAlive = true
+    won = false
+
+    cards = []
+    newCard(false)
+    newCard(false)
+    
+    renderGame()
+}
+
+function renderGame() {
+    if(getSum() < 21){
         console.log("Do you want to draw another card?")
         message = "Do you want to draw another card?"
-    }else if(sum === 21){
+    }else if(getSum() === 21){
         console.log("BlackJack!")
         message = "BloackJack!"
         won = true
@@ -31,6 +48,42 @@ function startGame() {
         isAlive = false
     }
     
-    cardsElement.textContent = "Cards: " +  card1 + " - " + card2
-    sumElement.textContent = "Sum: " + " " + sum
+    cardsElement.textContent = "Cards: "
+    for(let i = 0; i < cards.length; i++){
+        cardsElement.textContent += cards[i]
+        if(i != cards.length - 1){
+            cardsElement.textContent += ", "
+        }
+    }
+    sumElement.textContent = "Sum: " + " " + getSum()
+
+    document.getElementById("playerElement").textContent = player.name + ": $" + player.chips 
+}
+
+function newCard(process){
+    if(isAlive && !won){
+        let newCard = Math.floor(Math.random() * 13) + 1;
+        console.log("New card: " + newCard)
+        let value = 0;
+        if(newCard === 1){
+            value = 11
+        }else if(newCard > 10){
+            value = 10
+        }
+        else{
+            value = newCard
+        }
+        cards.push(value)
+        if(process){
+            renderGame()
+        }
+    }
+}
+
+function getSum(){
+    let sum = 0
+    for(let i = 0; i < cards.length; i++){
+        sum += cards[i]
+    }
+    return sum
 }
